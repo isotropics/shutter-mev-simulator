dc4e85529468d4ef1a608e735f5ba8a6fcd88fbe
Parimal999 <parimal.mj@isotrp.com>
2025-02-19 13:11:11 +0530

    Contracts update

--- contracts/interfaces/IUniswapV2Pair.sol
+++ contracts/interfaces/IUniswapV2Pair.sol
@@ -1,1 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
 
+interface IUniswapV2Pair {
+    function token0() external view returns (address);
+    function token1() external view returns (address);
+    function getReserves() external view returns (
+        uint112 reserve0,
+        uint112 reserve1,
+        uint32 blockTimestampLast
+    );
+    function price0CumulativeLast() external view returns (uint);
+    function price1CumulativeLast() external view returns (uint);
+    function totalSupply() external view returns (uint);
+    
+    event Sync(uint112 reserve0, uint112 reserve1);
+    event Swap(
+        address indexed sender,
+        uint amount0In,
+        uint amount1In,
+        uint amount0Out,
+        uint amount1Out,
+        address indexed to
+    );
+}
